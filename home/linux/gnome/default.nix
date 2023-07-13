8d51573945f3a6a3f2cfbcae34de0700dbe0bfe6
Taavi Ansper <taaviansperr@gmail.com>
2023-07-13 16:29:01 +0300

    Update gnome conf

--- home/linux/gnome/default.nix
+++ home/linux/gnome/default.nix
@@ -1,3 +1,55 @@
-{ ... }: {
-  
+{ pkgs, ... }:
+
+{
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Tela-Circle";
+      package = pkgs.tela-circle-icon-theme;
+    };
+    cursorTheme = {
+      name = "Capitaine-Cursors";
+      package = pkgs.capitaine-cursors-themed;
+    };
+    theme = {
+      name = "orchis";
+      package = pkgs.orchis-theme;
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=0
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=0
+      '';
+    };
+  };
+  home.sessionVariables.GTK_THEME = "orchis";
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+      ];
+      favorite-apps = [
+        "firefox.desktop"
+        "spotify.desktop"
+        "org.gnome.Terminal.desktop"
+        "org.gnome.Nautilus.desktop"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-light";
+    };
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "orchis";
+    };
+  };
+
+  home.packages = with pkgs; [
+    gnomeExtensions.user-themes
+    gnomeExtensions.sound-output-device-chooser
+  ];
 }
--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 {
+  # Enable gdm and gnome.
   services = {
     xserver = {
       enable = true;
@@ -14,4 +15,9 @@
       libinput.enable = true;
     };
   };
+  # Add gnome-tweaks.
+  environment.systemPackages = with pkgs; [
+    gnome.gnome-tweaks
+    gnomeExtensions.user-themes
+  ];
 }
