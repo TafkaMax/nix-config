c20966a1c534c58bf0bc7208666569cd5c8485b7
Taavi Ansper <taaviansperr@gmail.com>
2023-07-18 12:22:49 +0300

    Add gnome extensions which to download

--- home/linux/gnome/default.nix
+++ home/linux/gnome/default.nix
@@ -109,5 +109,7 @@ in
     gnomeExtensions.removable-drive-menu
     gnomeExtensions.workspace-indicator
     gnomeExtensions.appindicator
+    gnomeExtensions.pano
+    gnomeExtensions.top-panel-workspace-scroll
   ];
 }
