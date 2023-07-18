59e9102c2917014c9d5dc89d006335ae59e5b343
Taavi Ansper <taaviansperr@gmail.com>
2023-07-18 13:04:56 +0300

    Add extensions to gnome

--- home/linux/gnome/default.nix
+++ home/linux/gnome/default.nix
@@ -33,11 +33,22 @@ in
       disable-user-extensions = false;
       disabled-extensions = "disabled";
       enabled-extensions = [
+        #builtin extensions
         "user-theme@gnome-shell-extensions.gcampax.github.com"
         "places-menu@gnome-shell-extensions.gcampax.github.com"
         "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
         "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+        #explicitly added
         "no-overview@fthx"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "pano@elhan.io"
+        "hotedge@jonathan.jdoda.ca"
+        "pop-shell@system76.com"
+        "AlphabeticalAppGrid@stuarthayhurst"
+        "just-perfection-desktop@just-perfection"
+        "freon@UshakovVasilii_Github.yahoo.com"
       ];
       favorite-apps = [
         "firefox.desktop"
@@ -102,14 +113,19 @@ in
 
   home.packages = with pkgs; [
     gnomeExtensions.user-themes
-    # Sound output not supported yet.
-    #gnomeExtensions.sound-output-device-chooser
     gnomeExtensions.places-status-indicator
     gnomeExtensions.no-overview
     gnomeExtensions.removable-drive-menu
-    gnomeExtensions.workspace-indicator
+    gnomeExtensions.improved-workspace-indicator
     gnomeExtensions.appindicator
     gnomeExtensions.pano
-    gnomeExtensions.top-panel-workspace-scroll
+    gnomeExtensions.hot-edge
+    gnomeExtensions.pop-shell
+    gnomeExtensions.alphabetical-app-grid
+    gnomeExtensions.just-perfection
+    gnomeExtensions.freon
+    # Not supported yet.
+    #gnomeExtensions.sound-output-device-chooser
+    #gnomeExtensions.top-panel-workspace-scroll
   ];
 }
