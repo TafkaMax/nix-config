45ec5dad2178aa8fceee329ec632e34dbede9074
Taavi Ansper <taaviansperr@gmail.com>
2023-07-26 17:44:58 +0300

    Add kitty

--- modules/apps/kitty/default.nix
+++ modules/apps/kitty/default.nix
@@ -1,5 +1,19 @@
 { options, config, lib, pkgs, ... }:
 
+###########################################################
+#
+# Kitty Configuration
+#
+# Useful Hot Keys for Linux:
+#   1. New Tab: `ctrl + shift + t`
+#   2. Close Tab: `ctrl + shift + q`
+#   3. Switch Tab: `ctrl + shift + right` | `ctrl + shift + left`
+#   4. Increase Font Size: `ctrl + shift + =` | `ctrl + shift + +`
+#   5. Decrease Font Size: `ctrl + shift + -` | `ctrl + shift + _`
+#   6. And Other common shortcuts such as Copy, Paste, Cursor Move, etc.
+#
+###########################################################
+
 with lib;
 with lib.internal;
 let
@@ -11,5 +25,25 @@ in
   };
 
   config =
-    mkIf cfg.enable { environment.systemPackages = with pkgs; [ kitty ]; };
+    mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [ kitty ];
+
+      nixos-snowfall.home = {
+        extraOptions = {
+          programs.kitty = {
+            enable = true;
+            theme = "Relaxed Afterglow";
+            font = {
+              name = "JetBrainsMono Nerd Font";
+            };
+
+            settings = {
+              background_opacity = "0.95";
+              scrollback_lines = 10000;
+              enable_audio_bell = false;
+            };
+          };
+        };
+      };
+    };
 }
