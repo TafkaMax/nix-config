f6a1d9c2babbabf094f674600369e8ad6088a1de
Taavi Ansper <taaviansperr@gmail.com>
2023-07-26 18:03:07 +0300

    Add better git config

--- modules/tools/git/default.nix
+++ modules/tools/git/default.nix
@@ -19,22 +19,54 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ git ];
 
-    nixos-snowfall.home.extraOptions = {
-      programs.git = {
-        enable = true;
-        inherit (cfg) userName userEmail;
-        lfs = enabled;
-        #signing = {
-        #  key = cfg.signingKey;
-        #  signByDefault = mkIf gpg.enable true;
-        #};
-        extraConfig = {
-          init = { defaultBranch = "main"; };
-          pull = { rebase = true; };
-          push = { autoSetupRemote = true; };
-          core = { whitespace = "trailing-space,space-before-tab"; };
-          safe = {
-            directory = "${config.users.users.${user.name}.home}/work/config";
+
+    nixos-snowfall.home = {
+      # `programs.git` will generate the config file: ~/.config/git/config
+      # to make git use this config file, `~/.gitconfig` should not exist!
+      #
+      #    https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
+      activation.removeExistingGitconfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+        rm -f ~/.gitconfig
+      '';
+      extraOptions = {
+        programs.git = {
+          enable = true;
+          inherit (cfg) userName userEmail;
+          lfs = enabled;
+          #signing = {
+          #  key = cfg.signingKey;
+          #  signByDefault = mkIf gpg.enable true;
+          #};
+          extraConfig = {
+            init = { defaultBranch = "main"; };
+            pull = { rebase = true; };
+            push = { autoSetupRemote = true; };
+            core = { whitespace = "trailing-space,space-before-tab"; };
+            safe = {
+              directory = "${config.users.users.${user.name}.home}/work/config";
+            };
+            delta = {
+              enable = true;
+              options = {
+                features = "side-by-side";
+              };
+            };
+            aliases = {
+              # common aliases
+              br = "branch";
+              co = "checkout";
+              st = "status";
+              ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+              ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+              cm = "commit -m";
+              ca = "commit -am";
+              dc = "diff --cached";
+              amend = "commit --amend -m";
+
+              # aliases for submodule
+              update = "submodule update --init --recursive";
+              foreach = "submodule foreach";
+            };
           };
         };
       };
