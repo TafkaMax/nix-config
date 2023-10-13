88e4843eb8f2d295ce74313772d976bccfac23bc
Taavi Ansper <taaviansperr@gmail.com>
2023-10-13 21:32:44 +0300

    Add transmission.

---
+++ modules/apps/transmission/default.nix
@@ -0,0 +1,16 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let
+  cfg = config.nixos-snowfall.apps.transmission;
+in
+{
+  options.nixos-snowfall.apps.transmission = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Transmission bittorrent client.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ transmission ];
+  };
+}
