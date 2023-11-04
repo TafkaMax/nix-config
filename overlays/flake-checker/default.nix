627d985aa1521a9c1c9af5cd6be081a6bc089a67
Taavi Ansper <taaviansperr@gmail.com>
2023-11-04 12:45:31 +0200

    Remove flake-checker for now.

--- overlays/flake-checker/default.nix
+++ overlays/flake-checker/default.nix
@@ -1,6 +1,6 @@
-{ flake-checker, ... }:
-
-final: prev:
-{
-  inherit (flake-checker.packages.${prev.system}) flake-checker;
-}
+#{ flake-checker, ... }:
+#
+#final: prev:
+#{
+#  inherit (flake-checker.packages.${prev.system}) flake-checker;
+#}
