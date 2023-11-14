76f034c74ede671ffc22944546001c77226c7064
Taavi Ansper <taavi.ansper@cyber.ee>
2023-11-14 11:31:42 +0200

    Test gpg conf.

--- modules/nixos/security/gnome-keyring/default.nix
+++ modules/nixos/security/gnome-keyring/default.nix
@@ -15,5 +15,24 @@ in
       gnome.gnome-keyring
       gnome.libgnome-keyring
     ];
-  };
-}
+
+
+    nixos-snowfall.home = {
+      extraOptions = {
+
+        home.file = mkIf config.nixos-snowall.security.gpg.enable {
+          ".config/autostart/gnome-keyring-ssh.desktop" = {
+            text = ''
+              Hidden=true
+            '';
+          };
+
+          ".config/environment.d/10-ssh-auth-sock.conf" = {
+            text = (builtins.readFile /home/${config.nixos-snowfall.user.name}/.config/environment.d/10-ssh-auth-sock.conf) + ''
+              SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+            '';
+          };
+        };
+      };
+    };
+  }
