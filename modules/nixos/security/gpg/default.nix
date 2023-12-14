623cbd84d652bb97125ad434f5bf6d033dcc24af
Taavi Ansper <taavi.ansper@cyber.ee>
2023-12-14 11:09:39 +0200

    Update pinentry path.

--- modules/nixos/security/gpg/default.nix
+++ modules/nixos/security/gpg/default.nix
@@ -11,7 +11,7 @@ let
     enable-ssh-support
     default-cache-ttl 60
     max-cache-ttl 120
-    pinentry-program ${pkgs.pinentry-gnome}/bin/pinentry-gnome
+    pinentry-program ${pkgs.pinentry-gnome}/bin/pinentry
   '';
 
   guide = "${inputs.yubikey-guide}/README.md";
