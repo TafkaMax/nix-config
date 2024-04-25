a7e25a299c0997bbaef58231eba76be1541e8199
Taavi Ansper <taavi.ansper@cyber.ee>
2024-04-25 17:14:02 +0300

    Update python.

--- modules/nixos/tools/python/default.nix
+++ modules/nixos/tools/python/default.nix
@@ -14,9 +14,34 @@ in
       environment.systemPackages = with pkgs; [
         (python311.withPackages (ps:
           with ps; [
-            poetry
           ])
         )
+        poetry
       ];
     };
 }
+#  outputs = { self, nixpkgs, poetry2nix, ... } @ inputs:
+#    let
+#      system = "aarch64-darwin";
+#      pkgs = import nixpkgs {
+#        inherit system;
+#      };
+#     inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
+#      pythonEnv = mkPoetryEnv {
+#        python = pkgs.python311;
+#        projectDir = ./.;
+#      };
+#    in
+#    {
+#      devShells.${system}.default = pkgs.mkShell {
+#        packages = with pkgs; [
+#          ansible_2_14
+#          awscli2
+#          nodejs_18
+#          nodePackages.prettier
+#          poetry
+#          pythonEnv
+#          terraform
+#          terraform-ls
+#        ];
+#      };
