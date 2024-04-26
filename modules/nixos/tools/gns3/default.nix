bb15fc8bdf29ba438930d0dce069d0196b393361
Taavi Ansper <taavi.ansper@cyber.ee>
2024-04-26 10:37:55 +0300

    Remove wrappers and add ubridge logic to gns3.

--- modules/nixos/tools/gns3/default.nix
+++ modules/nixos/tools/gns3/default.nix
@@ -17,5 +17,14 @@ in
         gns3-gui
       ];
     };
+    security.wrappers = {
+      ubridge = {
+        source = "/run/current-system/sw/bin/ubridge";
+        capabilities = "cap_net_admin,cap_net_raw=ep";
+        owner = "root";
+        group = "ubridge";
+        permissions = "u+rx,g+x";
+      };
+    };
   };
 }
