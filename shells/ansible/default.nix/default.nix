571afd025875d2211ee4a22ac6e77c10970d10f7
Taavi Ansper <taavi.ansper@cyber.ee>
2024-05-07 15:50:55 +0300

    test out new shell

--- shells/ansible/default.nix/default.nix
+++ shells/ansible/default.nix/default.nix
@@ -11,7 +11,23 @@
 , ...
 }:
 
-stdenv.mkDerivation {
+pkgs.mkShell {
   # Create your shell
   LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
+  #LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib";
+  packages = with pkgs; [
+    python311
+    python311Packages.pip
+    poetry
+  ];
+  buildInputs = with pkgs; [
+    python311
+  ];
+  nativeBuildInputs = with pkgs; [
+    stdenv.cc.cc.lib
+  ];
+  shellHook = ''
+    poetry install
+    source $(poetry env info --path)/bin/activate
+  '';
 }
