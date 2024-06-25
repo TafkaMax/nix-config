ef671d842e0acea48a8e0379458de04de8c85004
Taavi Ansper <taavi.ansper@cyber.ee>
2024-06-25 16:48:42 +0300

    Revert logic for gns3-server module for now.

--- modules/nixos/tools/gns3/default.nix
+++ modules/nixos/tools/gns3/default.nix
@@ -17,19 +17,32 @@ in
         gns3-gui
       ];
     };
-    # Add gns3-server as service.
-    services.gns3-server = {
-      enable = true;
-      ubridge = {
-        enable = true;
-      };
-      auth = {
-        enable = true;
-        user = "gns3";
-        passwordFile = "/etc/nixos/modules/nixos/tools/gns3/gns3_password.txt";
-      };
-    };
-    # Add current user to ubridge group.
+    # Add gns3-server using the old way for now.
+    environment.systemPackages = with pkgs; [ gns3-server ubridge ];
+    users.groups.ubridge = { };
     users.users.${user.name}.extraGroups = [ "ubridge" ];
+    security.wrappers.ubridge = {
+      source = "${pkgs.ubridge}/bin/ubridge";
+      capabilities = "cap_net_admin,cap_net_raw=ep";
+      owner = "root";
+      group = "ubridge";
+      permissions = "u+rx,g+rx,o+rx";
+    };
+
+    # TODO don't currently add this.
+    # Add gns3-server as service.
+    #services.gns3-server = {
+    #  enable = true;
+    #  ubridge = {
+    #    enable = true;
+    #  };
+    #  auth = {
+    #    enable = true;
+    #    user = "gns3";
+    #    passwordFile = "/etc/nixos/modules/nixos/tools/gns3/gns3_password.txt";
+    #  };
+    #};
+    ## Add current user to ubridge group.
+    #users.users.${user.name}.extraGroups = [ "ubridge" ];
   };
 }
