b6955b10ceadd264463d6d2756b1c2957ebfd950
Taavi Ansper <taaviansperr@gmail.com>
2024-12-10 20:57:05 +0200

    Fix syntax

--- overlays/gnome-keyring/default.nix
+++ overlays/gnome-keyring/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 (final: prev: {
-  gnome = prev.gnome.overrideScope' (gfinal: gprev: {
+  gnome = prev.gnome.overrideScope (gfinal: gprev: {
     gnome-keyring = gprev.gnome-keyring.overrideAttrs (oldAttrs: {
       configureFlags = oldAttrs.configureFlags or [ ] ++ [
         "--disable-ssh-agent"
