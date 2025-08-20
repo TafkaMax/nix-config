e569908b07c6fb0d6902bf0281087f8c45634df7
Taavi Ansper <taaviansperr@gmail.com>
2025-08-20 13:22:11 +0300

    Also install handbrake with davinci

--- modules/nixos/apps/davinci-resolve/default.nix
+++ modules/nixos/apps/davinci-resolve/default.nix
@@ -11,6 +11,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ davinci-resolve ffmpeg ];
+    environment.systemPackages = with pkgs; [ davinci-resolve ffmpeg handbrake ];
   };
 }
