34a7db7c3db036bba7d8969e7c1c6c6e94aa6bf3
Taavi Ansper <taaviansperr@gmail.com>
2025-12-09 20:07:06 +0200

    wip

--- modules/nixos/cli-apps/neovim/default.nix
+++ modules/nixos/cli-apps/neovim/default.nix
@@ -30,7 +30,7 @@ in
       extraOptions = {
         # Use Neovim for Git diffs.
         #imports = [ ./neovim-config.nix ];
-        imports = [ inputs.nvf.homeManagerModules.default ./neovim-nvf-config.nix ];
+        imports = [ inputs.nvf.homeManagerModules.default ./neovim-nvf-config.nix ./editorconfig.nix ];
         programs.zsh.shellAliases.vimdiff = "nvim -d";
         programs.bash.shellAliases.vimdiff = "nvim -d";
         programs.fish.shellAliases.vimdiff = "nvim -d";
--- modules/nixos/cli-apps/neovim/neovim-nvf-config.nix
+++ modules/nixos/cli-apps/neovim/neovim-nvf-config.nix
@@ -6,74 +6,74 @@
     enable = true;
     defaultEditor = true;
     settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
-      vim.withNodeJs = true;
-      vim.globals = {
-        editorconfig = true;
-        mapleader = " ";
-      };
-      vim.filetree = {
-        nvimTree = {
+      vim = {
+        viAlias = false;
+        vimAlias = true;
+        withNodeJs = true;
+        globals = {
+          editorconfig = true;
+          mapleader = " ";
+        };
+        filetree = {
+          nvimTree = {
+            enable = true;
+            setupOpts = {
+              actions = {
+
+              };
+              git = {
+                enable = true;
+              };
+            };
+          };
+        };
+        treesitter = {
           enable = true;
+          grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
         };
-      };
-      vim.treesitter = {
-        enable = true;
-        grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
-      };
-      vim.lsp = {
-        enable = true;
-      };
-      vim.theme = {
-        enable = true;
-        name = "everforest"; #https://notashelf.github.io/nvf/options.html#opt-vim.theme.name
-        style = "soft";
-      };
-      vim.statusline = {
-        lualine = {
-          theme = "everforest"; #https://notashelf.github.io/nvf/options.html#opt-vim.statusline.lualine.theme
+        lsp = {
+          enable = true;
+
+          formatOnSave = true;
+          lspkind.enable = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          trouble.enable = true;
+          lspSignature.enable = false; # conflicts with blink in maximal
+          otter-nvim.enable = true;
+          nvim-docs-view.enable = true;
         };
-      };
-       vim.keymaps = [
-         {
-           key = "<leader>s";
-           mode = "n";
-           silent = false;
-           action = "nvim-tree.api.node.open.vertical";
-         }
-         {
-           key = "<leader>i";
-           mode = "i";
-           silent = false;
-           action = "nvim-tree.api.node.open.horizontal";
-         }
-       ];
-    };
-  };
-  editorconfig = {
-    enable = true;
-    settings = {
-      "*" = {
-        charset = "utf-8";
-        end_of_line = "lf";
-        trim_trailing_whitespace = true;
-        insert_final_newline = true;
-        max_line_width = 78;
-        indent_style = "space";
-        indent_size = 4;
-      };
-      "Makefile" = {
-        indent_style = "tab";
-      };
-      "*.{yml,yaml}" = {
-        indent_size = 2;
-      };
-      "*.json" = {
-        indent_size = 2;
-      };
-      "*.nix" = {
-        indent_size = 2;
+        theme = {
+          enable = true;
+          name = "everforest"; #https://notashelf.github.io/nvf/options.html#opt-vim.theme.name
+          style = "soft";
+        };
+        statusline = {
+          lualine = {
+            theme = "everforest"; #https://notashelf.github.io/nvf/options.html#opt-vim.statusline.lualine.theme
+          };
+        };
+        spellcheck = {
+          # TODO We need to install wordlist files, before enabling.
+          enable = false;
+          programmingWordlist.enable = false;
+        };
+        #vim.keymaps = [
+        #   {
+        #     key = "s";
+        #     mode = "n";
+        #     silent = false;
+        #     action = "nvim-tree.api.node.open.vertical";
+        #   }
+        #   {
+        #     key = "i";
+        #     mode = "n";
+        #     silent = false;
+        #     action = "nvim-tree.api.node.open.horizontal";
+        #   }
+        # ];
+        assistant = {
+        };
       };
     };
   };
