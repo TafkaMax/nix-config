be0f282024f8bd5aa9e8750e162aa9834a6b2450
Taavi Ansper <taaviansperr@gmail.com>
2026-04-27 19:00:04 +0300

    Remove nixos-generators

--- flake.nix
+++ flake.nix
@@ -51,12 +51,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # generate iso/qcow2/docker/... image from nixos configuration
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     # secrets management
     agenix = {
       url = "github:ryantm/agenix";
