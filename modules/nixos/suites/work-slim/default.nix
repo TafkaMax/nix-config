ac7a80a17bcce39db53226b1ef0733d8af8e6020
Taavi Ansper <taavi.ansper@cyber.ee>
2026-04-28 12:11:17 +0300

    Update archetype and suites for work

---
+++ modules/nixos/suites/work-slim/default.nix
@@ -0,0 +1,31 @@
+{ config, lib, namespace, ... }:
+
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.suites.work-slim;
+in
+{
+  options.${namespace}.suites.work-slim = with types; {
+    enable = mkBoolOpt false "Whether or not to enable work-slim PC configuration.";
+  };
+
+  config = mkIf cfg.enable {
+    ${namespace} = {
+      apps = {
+        element = enabled;
+        thunderbird = enabled;
+        zoom-us = enabled;
+        drawio = enabled;
+        yed = enabled;
+        gamja = enabled;
+        spotify = enabled;
+      };
+      tools = {
+        wireshark = enabled;
+        apache-directory-studio = enabled;
+        misc-work = enabled;
+      };
+    };
+  };
+}
