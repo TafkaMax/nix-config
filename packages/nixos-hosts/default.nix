4c013809104493bfe5bbc96b699c3e01e556d617
Taavi Ansper <taaviansperr@gmail.com>
2026-05-09 18:16:39 +0300

    Fix some stuff

--- packages/nixos-hosts/default.nix
+++ packages/nixos-hosts/default.nix
@@ -2,7 +2,7 @@
   lib,
   writeText,
   writeShellApplication,
-  substituteAll,
+  replaceVars,
   gum,
   inputs,
   hosts ? { },
@@ -13,9 +13,9 @@ let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.${namespace}) override-meta;
 
-  substitute = args: builtins.readFile (substituteAll args);
+  substitute = args: builtins.readFile (replaceVars ./nixos-hosts.sh { help = args.help; hosts = args.hosts; });
 
-  formatted-hosts = mapAttrsToList (name: host: "${name},${host.pkgs.system}") hosts;
+  formatted-hosts = mapAttrsToList (name: host: "${name},${host.pkgs.stdenv.hostPlatform.system}") hosts;
 
   hosts-csv = writeText "hosts.csv" ''
     Name,System
