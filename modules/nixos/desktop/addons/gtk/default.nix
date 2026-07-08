da09681f227f9d6cce2dc2c14a94ea39d04fac98
Taavi Ansper <taaviansperr@gmail.com>
2026-07-08 16:02:35 +0300

    Fix gtk warning

--- modules/nixos/desktop/addons/gtk/default.nix
+++ modules/nixos/desktop/addons/gtk/default.nix
@@ -59,6 +59,15 @@ in
           name = cfg.icon.name;
           package = cfg.icon.pkg;
         };
+
+        #  Fix evaluation warning by setting the gtk4 theme to null for now.
+        #  evaluation warning: The default value of `gtk.gtk4.theme` has changed from `config.gtk.theme` to `null`.
+        #              You are currently using the legacy default (`config.gtk.theme`) because `home.stateVersion` is less than "26.05".
+        #              To silence this warning and keep legacy behavior, set:
+        #                gtk.gtk4.theme = config.gtk.theme;
+        #              To adopt the new default behavior, set:
+        #                gtk.gtk4.theme = null;
+        gtk4.theme = null;
       };
     };
 
