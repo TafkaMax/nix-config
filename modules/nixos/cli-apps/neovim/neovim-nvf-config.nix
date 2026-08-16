320b2f72546b0f5421ea7b92f82ac4f096f745fe
Taavi Ansper <taavi.ansper@cyber.ee>
2026-08-16 22:22:22 +0300

    Comment out codewindow for now

--- modules/nixos/cli-apps/neovim/neovim-nvf-config.nix
+++ modules/nixos/cli-apps/neovim/neovim-nvf-config.nix
@@ -252,7 +252,7 @@ with lib.${namespace};
         minimap = {
           minimap-vim.enable = true;
           #{TODO} Change when https://github.com/NotAShelf/nvf/issues/1426 is fixed. Codewindow is a culprit.
-          codewindow.enable = false; # lighter, faster, and uses lua for configuration: https://github.com/gorbit99/codewindow.nvim?tab=readme-ov-file#configuration
+          #codewindow.enable = false; # lighter, faster, and uses lua for configuration: https://github.com/gorbit99/codewindow.nvim?tab=readme-ov-file#configuration
         };
 
         terminal = {
